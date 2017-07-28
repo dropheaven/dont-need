c2ba723901e7502213cb97d2f3bd64240a0a1298
Saad <Dropheaven@live.com>
2017-07-28 12:00:07 -0400

    email and password validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ActiveRecord::Base
   has_many :comments
   has_secure_password
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  before_save { self.email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { within: 6..20 }
+
 end
